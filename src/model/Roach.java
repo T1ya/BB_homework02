d0bc47544efb65811c25415dd850634e0ae1e47a
Ilya <yashchilan@gmail.com>
2025-06-08 22:42:57 +0200

    Lesson02 homework roach races sleeptime fix

--- src/model/Roach.java
+++ src/model/Roach.java
@@ -5,12 +5,6 @@ import java.util.Random;
 public class Roach implements Runnable {
     private final int num;
     private final int stages;
-    private static final int SLEEPMAX;
-
-    static {
-        Random random = new Random();
-        SLEEPMAX = 2 + random.nextInt(4);
-    }
 
     public Roach(int num, int stages) {
         this.num = num;
@@ -20,10 +14,11 @@ public class Roach implements Runnable {
 
     @Override
     public void run() {
+        Random random = new Random();
         for (int i = 0; i < stages; i++) {
             System.out.printf("Roach %d runs stage %d/%d\n", num, i, stages);
             try {
-                Thread.sleep(SLEEPMAX);
+                Thread.sleep(2 + random.nextInt(4));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
